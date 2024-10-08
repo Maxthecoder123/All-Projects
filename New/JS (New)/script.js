b3c5a588cf29a662d6e1501b85fb6ce4d8ba3779
Max <85294320+Maxthecoder123@users.noreply.github.com>
2024-10-08 19:08:34 +0100

    Create script.js

---
+++ New/JS (New)/script.js
@@ -0,0 +1,24 @@
+const character = "#"; // Symbol for pyramid
+const count = 10; // Layers 
+const rows = [];
+let inverted = false; // Upright or Upsidedown
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
